c56ba00776650a67f4635bc73ded5863189f0a55
Mohit <mjagtap@xpanxion.com>
2020-12-14 12:57:12 +0530

    code change

--- Function1.cs
+++ Function1.cs
@@ -27,7 +27,7 @@ namespace FunctionApp5
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully with deployment.";
 
             return new OkObjectResult(responseMessage);
         }
